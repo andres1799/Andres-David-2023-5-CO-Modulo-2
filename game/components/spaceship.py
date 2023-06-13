1c3184563d5bb72f19fbb10283ba6b1754918a87
andres1799 <andresdavid179921@hotmail.com>
2023-06-13 18:11:02 -0500

    Correccion-First-Class-Homework

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -4,6 +4,7 @@ from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT
 class Spaceship(Sprite):
     SPACESHIP_WIDTH = 40
     SPACESHIP_HEIGTH = 60
+    SPEED = 10
     HALF_SCREEN_HEIGHT = SCREEN_HEIGHT // 2
     X_POS = (SCREEN_WIDTH // 2) - SPACESHIP_WIDTH
     Y_POS = 500
@@ -23,20 +24,18 @@ class Spaceship(Sprite):
             self.move_down()
 
     def move_left(self):
-        if self.rect.left > -40:
-            self.rect.x -= 10
-        else:
-            self.rect.x += SCREEN_WIDTH + 40
+        self.rect.x -= self.SPEED
+        if self.rect.left < -self.SPACESHIP_WIDTH:
+            self.rect.x += SCREEN_WIDTH + self.SPACESHIP_WIDTH
     def move_right(self):
-        if self.rect.right < SCREEN_WIDTH + 40:
-            self.rect.x += 10
-        else:
-            self.rect.x -= SCREEN_WIDTH + 40
+        self.rect.x += self.SPEED
+        if self.rect.right > SCREEN_WIDTH + self.SPACESHIP_WIDTH:
+            self.rect.x -= SCREEN_WIDTH + self.SPACESHIP_WIDTH
     def move_up(self):
         if self.rect.y > self.HALF_SCREEN_HEIGHT:
-            self.rect.y -= 10
+            self.rect.y -= self.SPEED
     def move_down(self):
         if self.rect.y < SCREEN_HEIGHT - self.SPACESHIP_HEIGTH:
-            self.rect.y += 10
+            self.rect.y += self.SPEED
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
